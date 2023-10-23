e76f649c7cd79ce608d04456e80a96261e0dd9c0
Maxime Gaudin <me@maxime.ly>
2023-10-23 13:14:36 +0200

    Some comments here & there

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Quick & dirty dataframe cache to ensure you don't have to download & parse the file each time
 dataframes = {}
 
 
@@ -19,16 +20,16 @@ def getDbConnection():
     return conn
 
 
-def getDataframe(year, month):
+def getDataframeFromCacheOrDownload(year, month):
     key = "{}{}".format(year, month)
     try:
         return dataframes[key]
     except KeyError:
-        dataframes[key] = pd.read_csv(fetchFile(year, month), parse_dates=["timestamp"], index_col=["id"])
+        dataframes[key] = pd.read_csv(fetchFileIfNotAlreadyDownloaded(year, month), parse_dates=["timestamp"], index_col=["id"])
         return dataframes[key]
 
 
-def fetchFile(year, month):
+def fetchFileIfNotAlreadyDownloaded(year, month):
     outputFile = "./static/{}-{}.csv".format(year, f'{month:02}')
 
     if os.path.isfile(outputFile):
@@ -47,7 +48,9 @@ def runQuery():
     query = request.json['query']
 
     try:
-        df = getDataframe(year, month)
+        df = getDataframeFromCacheOrDownload(year, month)
+        # I did not had time to explore it further but this method is limited to one-liner
+        # Handling more complex pandas script would required an "eval" call
         return pd.eval(query).to_json()
     except HTTPError:
         return "Requested Year/Month does not exist", 404
