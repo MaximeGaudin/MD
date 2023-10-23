f8e2a54d94e6561d19c8627674386b147d511e0d
Maxime Gaudin <me@maxime.ly>
2023-10-23 11:30:12 +0200

    Full studio features

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
+import os.path
+import urllib.request
+from urllib.error import HTTPError
+
 import pandas as pd
 from flask import Flask, request
+
 app = Flask(__name__)
 
-df = pd.read_csv("./static/events.csv", parse_dates=["timestamp"], index_col=["id"])
+dataframes = {}
+
+
+def getDataframe(year, month):
+    key = "{}{}".format(year, month)
+    try:
+        return dataframes[key]
+    except KeyError:
+        dataframes[key] = pd.read_csv(fetchFile(year, month), parse_dates=["timestamp"], index_col=["id"])
+        return dataframes[key]
+
+
+def fetchFile(year, month):
+    outputFile = "./static/{}-{}.csv".format(year, f'{month:02}')
+
+    if os.path.isfile(outputFile):
+        return outputFile
+
+    url = "https://work-sample-mk.s3.amazonaws.com/{}/{}/events.csv".format(year, f'{month:02}')
+
+    urllib.request.urlretrieve(url, outputFile)
+    return outputFile
 
 
 @app.route('/query', methods=["POST"])
@@ -12,7 +38,10 @@ def runQuery():
     query = request.json['query']
 
     try:
+        df = getDataframe(year, month)
         return pd.eval(query).to_json()
+    except HTTPError:
+        return "Requested Year/Month does not exist", 404
     except Exception as error:
         return str(error), 400
 
--- webapp/src/pages/index.vue
+++ webapp/src/pages/index.vue
@@ -49,6 +49,7 @@
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         required
                         v-model="query"
+                        @keypress.enter.prevent="displayQueryResult"
               >
                 df.head()
               </textarea>
@@ -66,9 +67,29 @@
             {{ error }}
           </div>
 
-          <div v-if="queryResult" class="mt-8">
-            <PandaTable :queryResult="queryResult"
-            />
+          <div v-if="queryResult">
+            <div>
+              <label for="display" class="block text-sm font-medium leading-6 text-gray-900">Display</label>
+              <select id="display" name="display"
+                      v-model="display"
+                      class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <option v-for="display in displayModes" :value="display.value">{{ display.name }}</option>
+              </select>
+            </div>
+
+            <div v-if="queryResult && display == DisplayMode.TABLE" class="mt-8">
+              <PandaTable :queryResult="queryResult"/>
+            </div>
+
+            <div v-if="queryResult && display == DisplayMode.LIST" class="mt-8">
+              <ul>
+                <li v-for="key in Object.keys(queryResult)" :key="key">{{ key }} : {{ queryResult[key] }}</li>
+              </ul>
+            </div>
+
+            <div v-if="queryResult && display == DisplayMode.JSON" class="mt-8">
+              <pre>{{ JSON.stringify(queryResult, null, 2) }}</pre>
+            </div>
           </div>
         </div>
       </div>
@@ -102,6 +123,28 @@ const displayQueryResult = async () => {
       .then((r) => queryResult.value = r)
       .catch((e) => error.value = e.response.data)
 }
+
+enum DisplayMode {
+  TABLE,
+  JSON,
+  LIST,
+}
+
+const display = ref(DisplayMode.TABLE)
+const displayModes = [
+  {
+    name: "Table",
+    value: DisplayMode.TABLE
+  },
+  {
+    name: "List",
+    value: DisplayMode.LIST
+  },
+  {
+    name: "Json",
+    value: DisplayMode.JSON
+  },
+]
 </script>
 
 
